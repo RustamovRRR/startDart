629dcffb2b66075e6944190e79e35d9d84e4c8f7
RustamovRRR <rustamrrustamov@gmail.com>
2022-04-11 21:54:11 +0400

    using different variables

---
+++ lesson1/variables.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int age = 34;
+  double average = 1.5;
+  num smtSpec = 7.7;
+  String name = "Rustam";
+  var surname = "Rustamov";
+
+  print(age);
+  print(average);
+  print(smtSpec);
+  print(name);
+  print(surname);
+}
