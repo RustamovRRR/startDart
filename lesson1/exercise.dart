65ec5d0697ca901fdb983b934cb57e541718f324
RustamovRRR <rustamrrustamov@gmail.com>
2022-04-12 22:05:49 +0400

    overcome exercise

---
+++ lesson1/exercise.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  String name = "Rustam";
+  String surname = "Rustamov";
+  int age = 27;
+  print("my name is $name and my surname is $surname. I am $age years old");
+
+  const a = 5;
+  const b = 6;
+  const c = 7;
+  print("Ucbucagin birinci terefi $a, ikinci $b, ucuncu $c perimetri ise ${a+b+c} dir");
+}
