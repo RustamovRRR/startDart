e39858dad94f45b752766a00a3aa4203ae3383f1
RustamovRRR <rustamrrustamov@gmail.com>
2022-04-13 22:19:37 +0400

    understanding for in loop

--- section2/loopOperators.dart
+++ section2/loopOperators.dart
@@ -1,7 +1,13 @@
 void main(List<String> args) {
+  // for loop
   for (var i = 0; i < 10; i++) {
     if (i != 0 && i % 2 == 0) {
       print("cut saylar $i");
     }
+  }
+  // for in loop
+  List names = ['Rustam', "Harrison", "Clint"];
+  for (var name in names) {
+    print(name);
   }
 }
