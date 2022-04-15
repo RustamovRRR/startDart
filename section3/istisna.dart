d625889e8d58e5fb82e255a3854074e2542d32ec
RustamovRRR <rustamrrustamov@gmail.com>
2022-04-15 20:52:44 +0400

    understanding optional named parameters

--- section3/istisna.dart
+++ section3/istisna.dart
@@ -1,9 +1,17 @@
 void main(List<String> args) {
   int cem = sum(4, 3, 2);
   print(cem);
+  int hasil = multiply(b: 4, c: 2);
+  print(hasil);
 }
 
-// optional
+// optional parameters
 int sum(int a, int b, [int c = 0]) {
   return a + b + c;
+}
+
+// optional named parameters
+
+int multiply({int a = 1, int b = 1, int c = 1}) {
+  return a * b * c;
 }
