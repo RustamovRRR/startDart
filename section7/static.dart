a3312cc377dd94f23c54f312d1ecf24f2b9f0c97
RustamovRRR <rustamrrustamov@gmail.com>
2022-05-03 19:36:44 +0400

    understanding static keyword

---
+++ section7/static.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  print(Riyaziyyat.PI);
+  Riyaziyyat.className();
+}
+
+class Riyaziyyat {
+  int eded1;
+  int eded2;
+  static double PI = 3.14;
+  Riyaziyyat(this.eded1, this.eded2);
+
+  static void className() {
+    print("Riyaziyyat sinifi");
+  }
+}
