ab90c9121d0f3c9b9240ea24c1ca60c93aea8065
RustamovRRR <rustamrrustamov@gmail.com>
2022-05-03 20:01:09 +0400

    const vs final

---
+++ section7/finalVSconst.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  final String name = "John";
+  const String surname = "Doe";
+
+  final int eded = 12;
+
+  final list = [1, 2, 3];
+  list.add(12);
+
+  // constla add methodundan istifade etmek olmaz
+
+  final vaxt = DateTime.now();
+  print(vaxt);
+}
